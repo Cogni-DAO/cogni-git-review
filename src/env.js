ca750b7fbb63f45e3f251362d3ee987e8b6328c3
derekg1729 <derekgranito@gmail.com>
2025-10-17 18:41:13 -0700

    breaking: make gitlab secret required env too... both github and gitlab secrets required for the app to run. Temporary state, but doing it for now to fail fast

--- src/env.js
+++ src/env.js
@@ -54,7 +54,7 @@ const github = z.object({
 const gitlab = z.object({
   GITLAB_OAUTH_APPLICATION_ID: z.string().optional(),
   GITLAB_OAUTH_APPLICATION_SECRET: z.string().optional(),
-  WEBHOOK_SECRET_GITLAB: z.string().optional(),
+  WEBHOOK_SECRET_GITLAB: isDevelopmentMode ? z.string().optional() : z.string().min(1),
   WEBHOOK_PROXY_URL_GITLAB: urlOrUndef(),
 });
 
